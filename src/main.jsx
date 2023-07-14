7e5d7b9b0c747a56b39c0e82422a4581b7a2c32a
tanveer anjum <tanveerdar933@gmail.com>
2023-07-14 19:12:41 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import Root, { loader as rootLoader } from "./routes/root.jsx"
+import ErrorPage from './error-page.jsx'
+import Contact from './routes/contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
